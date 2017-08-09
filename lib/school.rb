86a42251ae9980da68b8d4eb3a81eb47d3855922
github username <github email address>
2017-08-09 18:07:35 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  attr_accessor :roster, :student, :age
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster.keys.include?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = [student]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+  @roster.each do |key, value|
+   value.sort!
+  end
+  end
+
+end
